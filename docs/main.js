c28d1234438addc697821ebe738e3703522e9f24
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2022-06-06 19:48:26 +0900

    Okay, implement displaying the prob

--- docs/main.js
+++ docs/main.js
@@ -2,7 +2,7 @@
 var makeLilyPadAt = function (_a, angle) {
     var x = _a[0], y = _a[1];
     var path = document.createElementNS('http://www.w3.org/2000/svg', "path");
-    path.setAttribute("transform", "\n\ttranslate(" + (PADDING + SPACING_CONSTANT * x) + " " + (PADDING + SPACING_CONSTANT * y) + ")\n\tscale(" + LILYPAD_RADIUS / 13 + ")\n\trotate(" + angle + ")\n\t");
+    path.setAttribute("transform", "\n\ttranslate(" + (OUTER_PADDING + SPACING_CONSTANT * x) + " " + (OUTER_PADDING + SPACING_CONSTANT * y) + ")\n\tscale(" + LILYPAD_RADIUS / 13 + ")\n\trotate(" + angle + ")\n\t");
     path.setAttribute('fill', '#66c810');
     path.setAttribute("d", "m -13 0 a 13 13 0 0 0 25 5 a 5 5 0 0 1 0 -10 a 13 13 0 0 0 -25 5");
     return path;
@@ -11,22 +11,22 @@ var makeGuidingLine = function (_a, _b) {
     var x1 = _a[0], y1 = _a[1];
     var x2 = _b[0], y2 = _b[1];
     var line = document.createElementNS('http://www.w3.org/2000/svg', "line");
-    line.setAttribute("x1", "" + (PADDING + SPACING_CONSTANT * x1));
-    line.setAttribute("y1", "" + (PADDING + SPACING_CONSTANT * y1));
+    line.setAttribute("x1", "" + (OUTER_PADDING + SPACING_CONSTANT * x1));
+    line.setAttribute("y1", "" + (OUTER_PADDING + SPACING_CONSTANT * y1));
     line.setAttribute('stroke', '#b4dded');
-    line.setAttribute("x2", "" + (PADDING + SPACING_CONSTANT * x2));
-    line.setAttribute("y2", "" + (PADDING + SPACING_CONSTANT * y2));
+    line.setAttribute("x2", "" + (OUTER_PADDING + SPACING_CONSTANT * x2));
+    line.setAttribute("y2", "" + (OUTER_PADDING + SPACING_CONSTANT * y2));
     return line;
 };
-var SPACING_CONSTANT = 30;
-var PADDING = 30;
-var LILYPAD_RADIUS = 15;
-var FROG_RADIUS = 12;
+var SPACING_CONSTANT = 40;
+var OUTER_PADDING = 30;
+var LILYPAD_RADIUS = 25;
+var FROG_RADIUS = 10;
 var createBackground = function () {
     var g = document.createElementNS('http://www.w3.org/2000/svg', "g");
     var pond_water = document.createElementNS('http://www.w3.org/2000/svg', "rect");
-    pond_water.setAttribute("width", "" + (PADDING * 2 + SPACING_CONSTANT * 4));
-    pond_water.setAttribute("height", "" + (PADDING * 2 + SPACING_CONSTANT * 4));
+    pond_water.setAttribute("width", "" + (OUTER_PADDING * 2 + SPACING_CONSTANT * 4));
+    pond_water.setAttribute("height", "" + (OUTER_PADDING * 2 + SPACING_CONSTANT * 4));
     pond_water.setAttribute("fill", "#01a4e7");
     g.appendChild(pond_water);
     g.appendChild(makeGuidingLine([0, 0], [4, 0]));
@@ -56,53 +56,75 @@ var createBackground = function () {
     g.appendChild(makeLilyPadAt([4, 4], -60)); // a3
     return g;
 };
-var drawFrogs = function (cs) {
-    var g = document.createElementNS('http://www.w3.org/2000/svg', "g");
-    for (var _i = 0, cs_1 = cs; _i < cs_1.length; _i++) {
-        var c = cs_1[_i];
+var frogImg = function () {
+    var frog = document.createElementNS('http://www.w3.org/2000/svg', "g");
+    frog.setAttribute("transform", "\n\t\t\tscale(1, 1.5)\n\t\t\tscale(" + FROG_RADIUS / 10 + ")");
+    var frog_bg = document.createElementNS('http://www.w3.org/2000/svg', "path");
+    frog_bg.setAttribute('fill', '#49692d');
+    frog_bg.setAttribute("d", "m -10 0 C -10 7 10 7 10 0 C 10 -2 9 -3 6 -4 C 7 -7 3 -8 2 -5 C 1 -6 -1 -6 -2 -5 C -3 -8 -7 -7 -6 -4 C -9 -3 -10 -2 -10 0");
+    var frog_eye = document.createElementNS('http://www.w3.org/2000/svg', "circle");
+    frog_eye.setAttribute("fill", "#000000");
+    frog_eye.setAttribute("cx", "4.5");
+    frog_eye.setAttribute("cy", "-4");
+    frog_eye.setAttribute("r", "1");
+    var frog_eye2 = document.createElementNS('http://www.w3.org/2000/svg', "circle");
+    frog_eye2.setAttribute("fill", "#000000");
+    frog_eye2.setAttribute("cx", "-4.5");
+    frog_eye2.setAttribute("cy", "-4");
+    frog_eye2.setAttribute("r", "1");
+    var frog_eye3 = document.createElementNS('http://www.w3.org/2000/svg', "circle");
+    frog_eye3.setAttribute("fill", "#FFA500");
+    frog_eye3.setAttribute("cx", "4");
+    frog_eye3.setAttribute("cy", "-4");
+    frog_eye3.setAttribute("r", "2");
+    var frog_eye4 = document.createElementNS('http://www.w3.org/2000/svg', "circle");
+    frog_eye4.setAttribute("fill", "#FFA500");
+    frog_eye4.setAttribute("cx", "-4");
+    frog_eye4.setAttribute("cy", "-4");
+    frog_eye4.setAttribute("r", "2");
+    var frog_mouth = document.createElementNS('http://www.w3.org/2000/svg', "path");
+    frog_mouth.setAttribute('stroke', '#000000');
+    frog_mouth.setAttribute('fill', 'transparent');
+    frog_mouth.setAttribute("d", "m-7 0C-3 3 3 3 7 0");
+    frog.appendChild(frog_bg);
+    frog.appendChild(frog_eye3);
+    frog.appendChild(frog_eye4);
+    frog.appendChild(frog_eye);
+    frog.appendChild(frog_eye2);
+    frog.appendChild(frog_mouth);
+    return frog;
+};
+var drawFrogs = function (fs) {
+    var frogs = document.createElementNS('http://www.w3.org/2000/svg', "g");
+    for (var _i = 0, fs_1 = fs; _i < fs_1.length; _i++) {
+        var f = fs_1[_i];
         var frog = document.createElementNS('http://www.w3.org/2000/svg', "g");
-        frog.setAttribute("transform", "\n\t\t\ttranslate(" + (PADDING + SPACING_CONSTANT * c[0]) + " " + (PADDING + SPACING_CONSTANT * c[1]) + ")\n\t\t\tscale(1, 1.5)\n\t\t\tscale(" + FROG_RADIUS / 10 + ")");
-        var frog_bg = document.createElementNS('http://www.w3.org/2000/svg', "path");
-        frog_bg.setAttribute('fill', '#49692d');
-        frog_bg.setAttribute("d", "m -10 0 C -10 7 10 7 10 0 C 10 -2 9 -3 6 -4 C 7 -7 3 -8 2 -5 C 1 -6 -1 -6 -2 -5 C -3 -8 -7 -7 -6 -4 C -9 -3 -10 -2 -10 0");
-        var frog_eye = document.createElementNS('http://www.w3.org/2000/svg', "circle");
-        frog_eye.setAttribute("fill", "#000000");
-        frog_eye.setAttribute("cx", "4.5");
-        frog_eye.setAttribute("cy", "-4");
-        frog_eye.setAttribute("r", "1");
-        var frog_eye2 = document.createElementNS('http://www.w3.org/2000/svg', "circle");
-        frog_eye2.setAttribute("fill", "#000000");
-        frog_eye2.setAttribute("cx", "-4.5");
-        frog_eye2.setAttribute("cy", "-4");
-        frog_eye2.setAttribute("r", "1");
-        var frog_eye3 = document.createElementNS('http://www.w3.org/2000/svg', "circle");
-        frog_eye3.setAttribute("fill", "#FFA500");
-        frog_eye3.setAttribute("cx", "4");
-        frog_eye3.setAttribute("cy", "-4");
-        frog_eye3.setAttribute("r", "2");
-        var frog_eye4 = document.createElementNS('http://www.w3.org/2000/svg', "circle");
-        frog_eye4.setAttribute("fill", "#FFA500");
-        frog_eye4.setAttribute("cx", "-4");
-        frog_eye4.setAttribute("cy", "-4");
-        frog_eye4.setAttribute("r", "2");
-        var frog_mouth = document.createElementNS('http://www.w3.org/2000/svg', "path");
-        frog_mouth.setAttribute('stroke', '#000000');
-        frog_mouth.setAttribute('fill', 'transparent');
-        frog_mouth.setAttribute("d", "m-7 0C-3 3 3 3 7 0");
-        frog.appendChild(frog_bg);
-        frog.appendChild(frog_eye3);
-        frog.appendChild(frog_eye4);
-        frog.appendChild(frog_eye);
-        frog.appendChild(frog_eye2);
-        frog.appendChild(frog_mouth);
-        g.appendChild(frog);
+        frog.setAttribute("transform", "translate(" + (OUTER_PADDING + SPACING_CONSTANT * f.coord[0]) + " " + (OUTER_PADDING + SPACING_CONSTANT * f.coord[1]) + ")");
+        if (f.prob != 1) {
+            var FROG_PROB_RADIUS = 13;
+            var frog_prob = document.createElementNS('http://www.w3.org/2000/svg', "path");
+            frog_prob.setAttribute('fill', "transparent");
+            frog_prob.setAttribute('stroke', f.color);
+            frog_prob.setAttribute("d", "m " + FROG_PROB_RADIUS + " 0 A " + FROG_PROB_RADIUS + " " + FROG_PROB_RADIUS + " 0 0 0 " + (FROG_PROB_RADIUS * Math.cos(Math.PI * 2 * f.prob)).toFixed(2) + " " + (FROG_PROB_RADIUS * Math.sin(Math.PI * 2 * f.prob)).toFixed(2));
+            frog_prob.setAttribute("stroke-width", "3");
+            frog.appendChild(frog_prob);
+        }
+        frog.appendChild(frogImg());
+        frogs.appendChild(frog);
     }
-    return g;
+    return frogs;
 };
 var main = function () {
     var background = createBackground();
     document.getElementById("board").innerHTML = ""; // clear
     document.getElementById("board").appendChild(background);
-    var frogs = drawFrogs([[0, 0], [2, 0], [4, 2], [2, 4]]);
+    var frogs = drawFrogs([
+        { coord: [0, 2], prob: 1, color: "transparent" },
+        { coord: [0, 4], prob: 0.5, color: "#aa5242" },
+        { coord: [1, 3], prob: 1, color: "transparent" },
+        { coord: [2, 2], prob: 0.5, color: "#aa5242" },
+        { coord: [2, 4], prob: 0.5, color: "#aa5242" },
+        { coord: [3, 3], prob: 0.5, color: "#aa5242" },
+    ]);
     document.getElementById("board").appendChild(frogs);
 };
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ const makeLilyPadAt = ([x, y]: Coord, angle: number) => {
 	const path = document.createElementNS('http://www.w3.org/2000/svg', "path");
 
 	path.setAttribute("transform", `
-	translate(${PADDING + SPACING_CONSTANT * x} ${PADDING + SPACING_CONSTANT * y})
+	translate(${OUTER_PADDING + SPACING_CONSTANT * x} ${OUTER_PADDING + SPACING_CONSTANT * y})
 	scale(${LILYPAD_RADIUS / 13})
 	rotate(${angle})
 	`);
@@ -15,24 +15,24 @@ const makeLilyPadAt = ([x, y]: Coord, angle: number) => {
 
 const makeGuidingLine = ([x1, y1]: Coord, [x2, y2]: Coord) => {
 	const line = document.createElementNS('http://www.w3.org/2000/svg', "line");
-	line.setAttribute("x1", `${PADDING + SPACING_CONSTANT * x1}`);
-	line.setAttribute("y1", `${PADDING + SPACING_CONSTANT * y1}`);
+	line.setAttribute("x1", `${OUTER_PADDING + SPACING_CONSTANT * x1}`);
+	line.setAttribute("y1", `${OUTER_PADDING + SPACING_CONSTANT * y1}`);
 	line.setAttribute('stroke', '#b4dded');
-	line.setAttribute("x2", `${PADDING + SPACING_CONSTANT * x2}`);
-	line.setAttribute("y2", `${PADDING + SPACING_CONSTANT * y2}`);
+	line.setAttribute("x2", `${OUTER_PADDING + SPACING_CONSTANT * x2}`);
+	line.setAttribute("y2", `${OUTER_PADDING + SPACING_CONSTANT * y2}`);
 	return line;
 }
 
-const SPACING_CONSTANT = 30;
-const PADDING = 30;
-const LILYPAD_RADIUS = 15;
-const FROG_RADIUS = 12;
+const SPACING_CONSTANT = 40;
+const OUTER_PADDING = 30;
+const LILYPAD_RADIUS = 25;
+const FROG_RADIUS = 10;
 
 const createBackground = () => {
 	const g = document.createElementNS('http://www.w3.org/2000/svg', "g");
 	const pond_water = document.createElementNS('http://www.w3.org/2000/svg', "rect");
-	pond_water.setAttribute("width", `${PADDING * 2 + SPACING_CONSTANT * 4}`)
-	pond_water.setAttribute("height", `${PADDING * 2 + SPACING_CONSTANT * 4}`)
+	pond_water.setAttribute("width", `${OUTER_PADDING * 2 + SPACING_CONSTANT * 4}`)
+	pond_water.setAttribute("height", `${OUTER_PADDING * 2 + SPACING_CONSTANT * 4}`)
 	pond_water.setAttribute("fill", `#01a4e7`)
 
 	g.appendChild(pond_water);
@@ -64,66 +64,92 @@ const createBackground = () => {
 	return g;
 }
 
-const drawFrogs = (cs: Coord[]) => {
-	const g = document.createElementNS('http://www.w3.org/2000/svg', "g");
-	for (const c of cs) {
-		const frog = document.createElementNS('http://www.w3.org/2000/svg', "g");
-		frog.setAttribute("transform", `
-			translate(${PADDING + SPACING_CONSTANT * c[0]} ${PADDING + SPACING_CONSTANT * c[1]})
+type Frog = { coord: Coord, prob: number, color: string };
+
+const frogImg = () => {
+	const frog = document.createElementNS('http://www.w3.org/2000/svg', "g");
+	frog.setAttribute("transform", `
 			scale(1, 1.5)
 			scale(${FROG_RADIUS / 10})`);
-		const frog_bg = document.createElementNS('http://www.w3.org/2000/svg', "path");
-		frog_bg.setAttribute('fill', '#49692d');
-		frog_bg.setAttribute("d", "m -10 0 C -10 7 10 7 10 0 C 10 -2 9 -3 6 -4 C 7 -7 3 -8 2 -5 C 1 -6 -1 -6 -2 -5 C -3 -8 -7 -7 -6 -4 C -9 -3 -10 -2 -10 0");
 
-		const frog_eye = document.createElementNS('http://www.w3.org/2000/svg', "circle");
-		frog_eye.setAttribute("fill", "#000000");
-		frog_eye.setAttribute("cx", "4.5");
-		frog_eye.setAttribute("cy", "-4");
-		frog_eye.setAttribute("r", "1");
+	const frog_bg = document.createElementNS('http://www.w3.org/2000/svg', "path");
+	frog_bg.setAttribute('fill', '#49692d');
+	frog_bg.setAttribute("d", "m -10 0 C -10 7 10 7 10 0 C 10 -2 9 -3 6 -4 C 7 -7 3 -8 2 -5 C 1 -6 -1 -6 -2 -5 C -3 -8 -7 -7 -6 -4 C -9 -3 -10 -2 -10 0");
 
-
-		const frog_eye2 = document.createElementNS('http://www.w3.org/2000/svg', "circle");
-		frog_eye2.setAttribute("fill", "#000000");
-		frog_eye2.setAttribute("cx", "-4.5");
-		frog_eye2.setAttribute("cy", "-4");
-		frog_eye2.setAttribute("r", "1");
-
-		const frog_eye3 = document.createElementNS('http://www.w3.org/2000/svg', "circle");
-		frog_eye3.setAttribute("fill", "#FFA500");
-		frog_eye3.setAttribute("cx", "4");
-		frog_eye3.setAttribute("cy", "-4");
-		frog_eye3.setAttribute("r", "2");
+	const frog_eye = document.createElementNS('http://www.w3.org/2000/svg', "circle");
+	frog_eye.setAttribute("fill", "#000000");
+	frog_eye.setAttribute("cx", "4.5");
+	frog_eye.setAttribute("cy", "-4");
+	frog_eye.setAttribute("r", "1");
 
 
-		const frog_eye4 = document.createElementNS('http://www.w3.org/2000/svg', "circle");
-		frog_eye4.setAttribute("fill", "#FFA500");
-		frog_eye4.setAttribute("cx", "-4");
-		frog_eye4.setAttribute("cy", "-4");
-		frog_eye4.setAttribute("r", "2");
+	const frog_eye2 = document.createElementNS('http://www.w3.org/2000/svg', "circle");
+	frog_eye2.setAttribute("fill", "#000000");
+	frog_eye2.setAttribute("cx", "-4.5");
+	frog_eye2.setAttribute("cy", "-4");
+	frog_eye2.setAttribute("r", "1");
 
-		const frog_mouth = document.createElementNS('http://www.w3.org/2000/svg', "path");
-		frog_mouth.setAttribute('stroke', '#000000');
-		frog_mouth.setAttribute('fill', 'transparent');
-		frog_mouth.setAttribute("d", "m-7 0C-3 3 3 3 7 0");
+	const frog_eye3 = document.createElementNS('http://www.w3.org/2000/svg', "circle");
+	frog_eye3.setAttribute("fill", "#FFA500");
+	frog_eye3.setAttribute("cx", "4");
+	frog_eye3.setAttribute("cy", "-4");
+	frog_eye3.setAttribute("r", "2");
 
 
-		frog.appendChild(frog_bg);
-		frog.appendChild(frog_eye3);
-		frog.appendChild(frog_eye4);
-		frog.appendChild(frog_eye);
-		frog.appendChild(frog_eye2);
-		frog.appendChild(frog_mouth);
-		g.appendChild(frog);
+	const frog_eye4 = document.createElementNS('http://www.w3.org/2000/svg', "circle");
+	frog_eye4.setAttribute("fill", "#FFA500");
+	frog_eye4.setAttribute("cx", "-4");
+	frog_eye4.setAttribute("cy", "-4");
+	frog_eye4.setAttribute("r", "2");
+
+	const frog_mouth = document.createElementNS('http://www.w3.org/2000/svg', "path");
+	frog_mouth.setAttribute('stroke', '#000000');
+	frog_mouth.setAttribute('fill', 'transparent');
+	frog_mouth.setAttribute("d", "m-7 0C-3 3 3 3 7 0");
+
+
+	frog.appendChild(frog_bg);
+	frog.appendChild(frog_eye3);
+	frog.appendChild(frog_eye4);
+	frog.appendChild(frog_eye);
+	frog.appendChild(frog_eye2);
+	frog.appendChild(frog_mouth);
+	return frog;
+}
+
+const drawFrogs = (fs: Frog[]) => {
+	const frogs = document.createElementNS('http://www.w3.org/2000/svg', "g");
+	for (const f of fs) {
+		const frog = document.createElementNS('http://www.w3.org/2000/svg', "g");
+		frog.setAttribute("transform", `translate(${OUTER_PADDING + SPACING_CONSTANT * f.coord[0]} ${OUTER_PADDING + SPACING_CONSTANT * f.coord[1]})`);
+		if (f.prob != 1) {
+			const FROG_PROB_RADIUS = 13;
+			const frog_prob = document.createElementNS('http://www.w3.org/2000/svg', "path");
+			frog_prob.setAttribute('fill', "transparent");
+			frog_prob.setAttribute('stroke', f.color);
+			frog_prob.setAttribute("d", `m ${FROG_PROB_RADIUS} 0 A ${FROG_PROB_RADIUS} ${FROG_PROB_RADIUS} 0 0 0 ${(FROG_PROB_RADIUS * Math.cos(Math.PI * 2 * f.prob)).toFixed(2)} ${(FROG_PROB_RADIUS * Math.sin(Math.PI * 2 * f.prob)).toFixed(2)}`)
+			frog_prob.setAttribute("stroke-width", "3");
+			frog.appendChild(frog_prob);
+		}
+		frog.appendChild(frogImg());
+
+		frogs.appendChild(frog);
 	}
-	return g;
+	return frogs;
 }
 
 const main = () => {
 	const background = createBackground();
 	document.getElementById("board")!.innerHTML = ""; // clear
 	document.getElementById("board")!.appendChild(background);
-	const frogs = drawFrogs([[0, 0], [2, 0], [4, 2], [2, 4]]);
+	const frogs = drawFrogs([
+		{ coord: [0, 2], prob: 1, color: "transparent" },
+		{ coord: [0, 4], prob: 0.5, color: "#aa5242" },
+		{ coord: [1, 3], prob: 1, color: "transparent" },
+		{ coord: [2, 2], prob: 0.5, color: "#aa5242" },
+		{ coord: [2, 4], prob: 0.5, color: "#aa5242" },
+		{ coord: [3, 3], prob: 0.5, color: "#aa5242" },
+	]);
 	document.getElementById("board")!.appendChild(frogs);
 
 }
